96623a813003ff099b2610435db7d0aa0166a538
Pobitrodeb <pobitrodn@gmail.com>
2023-02-05 22:12:45 +0600

    login and authorization done

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -20,4 +20,27 @@ class AuthController extends Controller
 
         return response()->json(['token' =>$token, 'users'=>$user]);
    }
+
+   public function login(Request $request){
+
+    $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+    ];
+
+    if(auth()->attempt($data)){
+        $token = auth()->user()->createToken('Token')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }else {
+        return response()->json(['error' => 'unauthorized'], 401);
+    }
+   }
+
+   public function userInfo() {
+
+    $user = auth()->user();
+    return response()->json(['user'=>$user],200);
+
+}
 }
